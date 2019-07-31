26c1017dd87a28f82eea3b6305fe131f5a305774
Christopher Smith <csmith4work@gmail.com>
2019-07-30 21:15:30 -0500

    safe state

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/store'
-import config from '../config'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/store';
+import vuetify from './plugins/vuetify';
 
-console.log(config.currentEnv());
-console.log(config.currentEnvAPI());
-
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/store/actions.js
+++ src/store/actions.js
@@ -8,12 +8,10 @@ const keysRoute = "keys/";
 
 const getUploadEvents = async ({commit}) => {
     const events = await axios.get(apiUrl + eventRoute);
-    console.log(events);
 };
 
 const getVideoKeys = async ({commit}) => {
     const keys = await axios.get(apiUrl + keysRoute);
-    console.log(keys);
 };
 
 export default {

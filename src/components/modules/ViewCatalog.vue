bf9c8f16bf94c3e56777082bbe9a54d1c0366f3f
Christopher Smith <csmith4work@gmail.com>
2019-08-17 10:21:05 -0500

    Set up the View Catalog page

--- src/components/modules/ViewCatalog.vue
+++ src/components/modules/ViewCatalog.vue
@@ -1,11 +1,27 @@
 <template>
-    <div>
-        <h1>View Catalog</h1>
-        <h3>This is the page where you view the entire catalog that has been uploaded to the backend storage.
-             You can check to see what the file names of the videos are, 
-             and whether or not they have been incorperated into a project.
-        </h3>
-        <HomeButton />
+    <div class="container">
+        <div class="container-header">
+            <h1 class="page-header">View Catalog</h1>
+        </div>
+        <div class="container-body">
+            <h3>
+                This is the page to view the entire catalog that has been uploaded to backend storage.
+                You can check to see what the file names of the videos are, preview a video,  
+                and check whether or not they have been incorperated into a project.
+            </h3>
+            <div class="table-container">
+                <table>
+                   <tr v-for="item in this.keys" v-bind:key="item.id">
+                       <td>
+                           <p>
+                               {{ item.key }}
+                           </p>
+                       </td>
+                   </tr>
+                </table>
+            </div>
+            <HomeButton />
+        </div>
     </div>
 </template>
 
@@ -15,5 +31,47 @@ export default {
     components: {
         HomeButton,
     },
+    computed: {
+        keys() {
+            return this.$store.state.keys;
+        },
+    },
+    async mounted() {
+        await this.$store.dispatch('getVideoKeys');
+    },
 }
 </script>
+
+<style scoped>
+.container{
+    width: 70%;
+    height: auto;
+    margin: auto;
+    padding: 15px;
+}
+
+.page-header{
+    width: 88%;
+    height: auto;
+    margin: auto;
+}
+
+.container-body{
+    width: 90%;
+    height: auto;
+    margin: auto;
+    word-wrap: normal;
+
+}
+
+.table-container{
+    width: auto;
+    height: auto;
+    margin: auto;
+    padding-bottom: 35px;
+}
+
+table{
+    margin: auto;
+}
+</style>
